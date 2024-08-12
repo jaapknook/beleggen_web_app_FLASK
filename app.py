b0ebd88d3ac6b4ab656dc5ead6297489d1a0577e
jaapknook <jaapknook@live.nl>
2024-08-12 23:23:42 +0200

    replace SQLalchemy

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 import os
 from werkzeug.utils import secure_filename
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'uploads'
-file_path = os.path.abspath(os.getcwd()) + r"\instance\users.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///' + file_path
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-   
-db = SQLAlchemy(app)
+app.config["MONGO_URI"] = "mongodb+srv://jaapknook:AW539wrGEaoq97XY@cluster0.n84sjfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -20,20 +19,18 @@ login_manager.login_view = 'login'
 # Ensure upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
-    files = db.relationship('File', backref='owner', lazy=True)
-
-class File(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(300), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class User(UserMixin):
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if user:
+        return User(str(user['_id']), user['username'], user['password'])
+    return None
 
 @app.route('/')
 def index():
@@ -49,9 +46,10 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
+        user = mongo.db.users.find_one({"username": username})
+        if user and bcrypt.check_password_hash(user['password'], password):
+            user_obj = User(str(user['_id']), user['username'], user['password'])
+            login_user(user_obj)
             return redirect(url_for('upload'))
         else:
             flash('Invalid credentials', 'danger')
@@ -66,9 +64,10 @@ def register():
         username = request.form['username']
         password = request.form['password']
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        user = User(username=username, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
+        user_id = mongo.db.users.insert_one({
+            "username": username,
+            "password": hashed_password
+        }).inserted_id
         flash('Registration successful. Please log in.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html')
@@ -93,9 +92,10 @@ def upload():
             user_folder = os.path.join(app.config['UPLOAD_FOLDER'], current_user.username)
             os.makedirs(user_folder, exist_ok=True)
             file.save(os.path.join(user_folder, filename))
-            new_file = File(filename=filename, owner=current_user)
-            db.session.add(new_file)
-            db.session.commit()
+            mongo.db.files.insert_one({
+                "filename": filename,
+                "user_id": ObjectId(current_user.id)
+            })
             return redirect(url_for('view_files'))
 
     return render_template('upload.html')
@@ -103,7 +103,7 @@ def upload():
 @app.route('/files')
 @login_required
 def view_files():
-    files = File.query.filter_by(owner=current_user).all()
+    files = mongo.db.files.find({"user_id": ObjectId(current_user.id)})
     return render_template('view_files.html', files=files)
 
 @app.route('/uploads/<username>/<filename>')
@@ -114,6 +114,4 @@ def uploaded_file(username, filename):
     return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], username), filename)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
